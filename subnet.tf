e5e22b5d995735eb2964bd735b470975b1779cb9
Prathamesh Shukla <shukla.prath@northeastern.edu>
2025-02-19 11:05:12 -0500

    testing

--- subnet.tf
+++ subnet.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "private_subnets" {
 
   count             = length(var.private_subnet_cidrs)
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = var.private_subnet_cidrs[count.index]
+  cidr_block= var.private_subnet_cidrs[count.index]
   availability_zone = element(data.aws_availability_zones.available.names, count.index % length(var.private_subnet_cidrs))
 
   tags = {
