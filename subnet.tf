786ba85cb62d62bb5e7e9986aa12b63b2e3b62f2
Prathamesh Shukla <shukla.prath@northeastern.edu>
2025-02-24 15:57:43 -0500

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public_subnets" {
 
   count                   = length(var.public_subnet_cidrs)
-  vpc_id                  = aws_vpc.my_vpc.id
+  vpc_id                  = aws_vpc.my_vpc.bsad
   cidr_block              = var.public_subnet_cidrs[count.index]
   availability_zone       = element(data.aws_availability_zones.available.names, count.index % length(var.public_subnet_cidrs))
   map_public_ip_on_launch = true
