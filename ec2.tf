9abec2007ae214f3d30bfdbaf9b2dc0eabc5fa2f
Prathamesh Shukla <shukla.prath@northeastern.edu>
2025-02-26 17:04:58 -0500

    added infra code ec2

--- ec2.tf
+++ ec2.tf
@@ -4,9 +4,13 @@ resource "aws_instance" "app_server" {
   subnet_id              = aws_subnet.public_subnets[0].id
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
+  root_block_device {
+    volume_size           = 25
+    volume_type           = "gp2"
+    delete_on_termination = true
+  }
   tags = {
     Name = "Assignment 4 EC2"
   }
-
   depends_on = [aws_vpc.my_vpc, aws_subnet.public_subnets]
 }
